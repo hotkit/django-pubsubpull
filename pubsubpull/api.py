a192d54aff60a87c8afce80d2e1496187f80762e
Kirit Sælensminde <k@kirit.com>
2015-05-04 17:01:19 +0700

    Call the new API.

--- pubsubpull/api.py
+++ pubsubpull/api.py
@@ -1,6 +1,5 @@
 
 
-def pull():
+def pull(model, callback):
     """Start a job pulling data from one service to this one.
     """
-    
--- pubsubpull/tests/test_pull.py
+++ pubsubpull/tests/test_pull.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 from pubsubpull.api import pull
+
+
+def job():
+    pass
+
+
+class TestPullStarts(TestCase):
+    def test_pull(self):
+        pull('slumber://test/Instance/', 'pubsubpull.tests.test_pull.job')
