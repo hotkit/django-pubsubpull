b2808f1e9843b92b0c054101b3a8602cc7c482f7
Kirit Sælensminde <k@kirit.com>
2015-05-05 15:24:00 +0700

    Make sure that our instance is processed.

--- pubsubpull/async.py
+++ pubsubpull/async.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 
 from async.api import schedule
 from datetime import timedelta
+from slumber.connector.api import get_model
+from slumber.connector.ua import get
 
 
 try:
@@ -13,8 +15,12 @@ except ImportError: # pragma: no cover
     from datetime import datetime as timezone
 
 
-def pull_monitor(model, callback, delay=dict(minutes=1)):
+def pull_monitor(model_url, callback, delay=dict(minutes=1)):
     """Used to look for instances that need to be pulled.
     """
+    model = get_model(model_url)
+    _, json = get(model._operations['instances'])
+    for item in json['page']:
+        schedule(callback, args=[item['data']])
     run_after = timezone.now() + timedelta(**delay)
-    schedule('pubsubpull.async.pull_monitor', run_after=run_after, args=[model, callback])
+    schedule('pubsubpull.async.pull_monitor', run_after=run_after, args=[model_url, callback])
--- pubsubpull/tests/test_pull.py
+++ pubsubpull/tests/test_pull.py
@@ -1,17 +1,27 @@
 from async.models import Job
 
 from django.core import management
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from pubsubpull.api import pull
+from slumber import data_link
+from slumber.connector.ua import for_user
 from slumber_examples.models import Pizza
 
 
-def job():
-    pass
+def job(url):
+    TestPullStarts.URLS.add(url)
 
 
 class TestPullStarts(TestCase):
+    def setUp(self):
+        self.service = User.objects.create(username=settings.SLUMBER_SERVICE,
+            is_active=True, is_staff=True, is_superuser=True,
+            password=settings.SECRET_KEY)
+        TestPullStarts.URLS = set()
+
     def test_empty_pull(self):
         pull('slumber://pizza/slumber_examples/Pizza/', 'pubsubpull.tests.test_pull.job')
         self.assertEquals(Job.objects.count(), 1)
@@ -20,7 +30,8 @@ class TestPullStarts(TestCase):
 
     def test_pull_one(self):
         pizza = Pizza.objects.create(name="Vegetarian")
-        pull('slumber://slumber_examples/Pizza/', 'pubsubpull.tests.test_pull.job')
+        pull('slumber://pizza/slumber_examples/Pizza/', 'pubsubpull.tests.test_pull.job')
         self.assertEquals(Job.objects.count(), 1)
         management.call_command('flush_queue')
         self.assertEquals(Job.objects.count(), 3)
+        self.assertIn(data_link(pizza), self.URLS)
