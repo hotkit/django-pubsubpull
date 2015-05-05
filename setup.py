c7b4162967d6d1fe4ae9476de744f1a55788fb27
Kirit Sælensminde <k@kirit.com>
2015-05-05 17:14:15 +0700

    Include setup.py.

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+def read(fname1, fname2):
+    if os.path.exists(fname1):
+        fname = fname1
+    else:
+        fname = fname2
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-pubsubpull",
+    version = "0.0.0.1",
+    author = "Kirit Saelensminde",
+    author_email = "kirit@felspar.com",
+    url='https://github.com/KayEss/django-pubsubpull',
+    description = ("Pub/sub and pull for Django"),
+    long_description = read('README','README.md'),
+    license = "Boost Software License - Version 1.0 - August 17th, 2003",
+    keywords = "django rest data pub-sub pull",
+    packages = [
+        'pubsubpull', 'pubsubpull.operations', 'pubsubpull.tests'],
+    install_requires = [
+        'django-slumber', 'django-async'],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved",
+    ],
+)
