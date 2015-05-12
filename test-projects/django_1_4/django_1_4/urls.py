be4f3cd13d66c14fb9c13e6fdb57fd1ed89eede6
Kirit Sælensminde <k@kirit.com>
2015-05-12 12:43:38 +0700

    Enable admin in the 1.4 project.

--- test-projects/django_1_4/django_1_4/urls.py
+++ test-projects/django_1_4/django_1_4/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
     # Enable Slumber
     (r'^slumber/', include('slumber.urls')),
 )
