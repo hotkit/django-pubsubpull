383de24df6fa7eb431fdfd3ae2e6b35c4e7d962b
Kirit Sælensminde <k@kirit.com>
2016-10-21 11:36:38 +0700

    Split the middleware into two parts

--- pubsubpull/middleware.py
+++ pubsubpull/middleware.py
@@ -13,24 +13,29 @@ from django.db import connection
 from pubsubpull.models import Request
 
 
-class RequestTracker:
+class RequestUser:
     """
-        Used to record requests and tie them in to database changes.
+        Records the current user in the session
     """
     def process_request(self, request):
         if request.user.is_authenticated():
             user = request.user
         else:
             user = None
-        log = Request.objects.create(user=user,
-            method=request.method, path=request.path)
+        log = Request.objects.create(user=user, method=request.method,
+            path=request.path)
         connection.cursor().execute("SET LOCAL pubsubpull.request_id=%s;" % log.id)
         request.pubsubpull = dict(log=log)
 
+
+class RequestTracker(RequestUser):
+    """
+        Used to record the result of requests.
+    """
     def process_response(self, request, response):
-        # if getattr(request, 'pubsubpull' , None):
-        #     log = request.pubsubpull['log']
-        #     log.duration = (timezone.now() - log.started).seconds
-        #     log.status = response.status_code
-        #     log.save()
+        if getattr(request, 'pubsubpull' , None):
+            log = request.pubsubpull['log']
+            log.duration = (timezone.now() - log.started).seconds
+            log.status = response.status_code
+            log.save()
         return response
